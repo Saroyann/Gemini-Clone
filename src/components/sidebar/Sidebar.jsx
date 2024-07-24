b1d03a3f384c629b0c957a1cfdf52c925f097466
Yiim <ilyimyan@gmail.com>
2024-07-24 12:27:43 +0800

    initial commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
 
-const [extended, setExtended] = useState(false)
+    const [extended, setExtended] = useState(false)
 
     return (
         <div className='sidebar'>
@@ -14,30 +14,34 @@ const [extended, setExtended] = useState(false)
 
                 <div className="new-chat">
                     <img src={assets.plus_icon} />
-                    {extended?<p>New Chat</p>:null}
+                    {extended ? <p>New Chat</p> : null}
                 </div>
 
-                <div className="recent">
-                    <p className="recent-title">Recent</p>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} />
-                        <p>what is react ...</p>
+                {extended
+                    ?
+                    <div className="recent">
+                        <p className="recent-title">Recent</p>
+                        <div className="recent-entry">
+                            <img src={assets.message_icon} />
+                            <p>what is react ...</p>
+                        </div>
                     </div>
-                </div>
+                    : null
+                }
             </div>
 
             <div className="bottom">
                 <div className="bottom-item recent-entry">
                     <img src={assets.question_icon} />
-                    <p>Help</p>
+                    {extended? <p>Help</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.history_icon} />
-                    <p>Activity</p>
+                    {extended ?<p>Activity</p> : null}
                 </div>
                 <div className="bottom-item recent-entry">
                     <img src={assets.setting_icon} />
-                    <p>Settings</p>
+                    {extended ? <p>Settings</p> : null}
                 </div>
             </div>
         </div>
